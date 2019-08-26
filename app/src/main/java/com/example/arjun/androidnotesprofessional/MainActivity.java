5a131386423dc97ef5e23ee2b5e5e434b32dc025
arjun <arjunthestar007@gmail.com>
2019-08-26 08:21:31 -0700

    Spannable font:  set a different font size to some portion of text, a RelativeSizeSpan

--- app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
+++ app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
@@ -6,23 +6,36 @@ import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+    TextView textView1, textView2;
+    String firstWord, lastWord;
+    Spannable spannable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.t1);
+        textView1 = findViewById(R.id.t1);
+        textView2 = findViewById(R.id.t2);
+
+        firstWord = "Spannable";
+        lastWord = "Color";
 
 
-        String firstWord="Hello",lastWord="world";
-
-        Spannable spannable = new SpannableString(firstWord + lastWord);
+        spannable = new SpannableString(firstWord + lastWord);
         spannable.setSpan(new ForegroundColorSpan(Color.RED), 0, firstWord.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         spannable.setSpan(new ForegroundColorSpan(Color.BLUE), firstWord.length(), firstWord.length() + lastWord.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        textView.setText(spannable);
+        textView1.setText(spannable);
+
+        firstWord = "Spannable";
+        lastWord = "Font";
+
+        spannable = new SpannableString(firstWord + lastWord);
+        spannable.setSpan(new RelativeSizeSpan(1.1f), 0, firstWord.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE); // set size
+        spannable.setSpan(new RelativeSizeSpan(0.8f), firstWord.length(), firstWord.length() + lastWord.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE); // set size
+        textView2.setText(spannable);
     }
 }
