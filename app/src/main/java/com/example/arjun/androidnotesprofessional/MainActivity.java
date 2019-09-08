a7e2fb8c2994cadc2dc6aed97ad8c9d41a24508f
arjun <arjunthestar007@gmail.com>
2019-09-08 07:04:11 -0700

    Webview loadurl from localurl

--- app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
+++ app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.arjun.androidnotesprofessional;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.webview);
+        mWebView.setWebChromeClient(new ChromeClient());
+        mWebView.loadUrl("file:///android_asset/index.html");
     }
 }
