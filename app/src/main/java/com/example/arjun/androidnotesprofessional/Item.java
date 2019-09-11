a977a8f776b2be402516933e3e5dbbed4ada36ab
arjun <arjunthestar007@gmail.com>
2019-09-10 19:35:23 -0700

    Databinding #Basic text field binding

---
+++ app/src/main/java/com/example/arjun/androidnotesprofessional/Item.java
@@ -0,0 +1,12 @@
+package com.example.arjun.androidnotesprofessional;
+
+public class Item {
+
+    public String name;
+    public String description;
+
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
--- app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
+++ app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.arjun.androidnotesprofessional;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.arjun.androidnotesprofessional.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding=DataBindingUtil.setContentView(this,R.layout.activity_main);
+        Item item = new Item("Example item", "This is an example item.");
+        binding.setItem(item);
     }
 }
