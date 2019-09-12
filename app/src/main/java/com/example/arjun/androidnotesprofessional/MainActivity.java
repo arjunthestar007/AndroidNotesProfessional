3ec0b8a9ef421b3a6142f1c454e8e263e55684e3
arjun <arjunthestar007@gmail.com>
2019-09-12 10:20:30 -0700

    buildtype,productflavour,siginconfig,buildConfigField,resvalue

--- app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
+++ app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.arjun.androidnotesprofessional;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView=findViewById(R.id.textview1);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        String buildDate = dateFormat.format(new Date(BuildConfig.BUILD_DATE));
+        Log.d("build date", buildDate);
+
+        // do something depending whether this is a productive build
+        if (BuildConfig.IS_PRODUCTION) {
+            mTextView.setText(String.valueOf(BuildConfig.IS_PRODUCTION));
+        } else {
+            mTextView.setText(String.valueOf(BuildConfig.IS_PRODUCTION));
+        }
+
+        getResources().getString(R.string.app_name1);
     }
 }
