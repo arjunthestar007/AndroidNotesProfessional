9f62481de740b224add02ff8fd802f43b45f3504
arjun <arjunthestar007@gmail.com>
2019-09-16 19:41:31 -0700

    Textview added

--- app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
+++ app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.arjun.androidnotesprofessional;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView=findViewById(R.id.textview);
     }
 }
