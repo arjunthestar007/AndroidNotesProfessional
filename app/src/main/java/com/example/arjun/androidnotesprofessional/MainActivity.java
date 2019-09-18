353d2fc03c87ebf2659ada8cd3ab57ee80d82b6c
arjun <arjunthestar007@gmail.com>
2019-09-17 19:53:51 -0700

    Parse simple JSON object

--- app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
+++ app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
@@ -10,13 +10,14 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     TextView mTextView;
+    TextView mParsedTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = findViewById(R.id.textview);
-
+        mParsedTextView=findViewById(R.id.textView2);
         // Create a new instance of a JSONObject
         JSONObject object = new JSONObject();
         // Create a new instance of a JSONArray
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             // With put you can add a name/value pair to the JSONObject
-            object.put("name", "test");
+            object.put("name", "Arjun");
             object.put("content", "Hello World!!!1");
             object.put("year", 2016);
             object.put("value", 3.23);
@@ -45,6 +46,27 @@ public class MainActivity extends AppCompatActivity {
 
         final String json = object.toString();
         mTextView.setText(json);
+
+        StringBuilder str=new StringBuilder();
+        try {
+            JSONObject jsonObject=new JSONObject(json);
+            String name = jsonObject.getString("name");
+            String content = jsonObject.getString("content");
+            int year = jsonObject.getInt("year");
+            str.append("Name :"+name);
+
+            JSONArray names = jsonObject.getJSONArray("my_loc_array");
+
+            for (int i = 0; i < names.length(); i++) {
+                str.append("Location :"+names.get(i));
+
+
+
+            }
+            mParsedTextView.setText(str);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 }
