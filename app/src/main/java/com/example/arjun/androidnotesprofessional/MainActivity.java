a4bebde55cf1ecf687a6fe03fb5cf360cc997981
arjun <arjunthestar007@gmail.com>
2019-09-17 19:28:21 -0700

    Creating a simple JSON object

--- app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
+++ app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
     TextView mTextView;
 
@@ -11,6 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView=findViewById(R.id.textview);
+        mTextView = findViewById(R.id.textview);
+
+        // Create a new instance of a JSONObject
+        JSONObject object = new JSONObject();
+        try {
+            // With put you can add a name/value pair to the JSONObject
+            object.put("name", "test");
+            object.put("content", "Hello World!!!1");
+            object.put("year", 2016);
+            object.put("value", 3.23);
+            object.put("member", true);
+            object.put("null_value", JSONObject.NULL);
+            // Calling toString() on the JSONObject returns the JSON in string format.
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        final String json = object.toString();
+        mTextView.setText(json);
+
     }
 }
