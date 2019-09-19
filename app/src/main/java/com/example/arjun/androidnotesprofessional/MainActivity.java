171614601a27c9c331f1015c4c3bcda324819ff5
arjun <arjunthestar007@gmail.com>
2019-09-19 08:57:13 -0700

    Check for the existence of fields on JSON

--- app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
+++ app/src/main/java/com/example/arjun/androidnotesprofessional/MainActivity.java
@@ -18,57 +18,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mTextView = findViewById(R.id.textview);
         mParsedTextView = findViewById(R.id.textView2);
-        // Create a new instance of a JSONObject
-        JSONObject object = new JSONObject();
-        // Create a new instance of a JSONArray
-        JSONArray array = new JSONArray();
-        // With put() you can add a value to the array.
-        JSONObject object1 = new JSONObject();
+
+        String jsonStr = "{name:james}";
+        String name=null, surname;
+
+        JSONObject json = null; // Check if the field "name" is present String name, surname;
         try {
-            object1.put("loc1", "tambaram");
-            object1.put("loc2", "perugalathur");
+            json = new JSONObject(jsonStr);
 
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        array.put(object1);
-
-
-        try {
-            // With put you can add a name/value pair to the JSONObject
-            object.put("name", "Arjun");
-            object.put("content", "Hello World!!!1");
-            object.put("year", 2016);
-            object.put("value", 3.23);
-            object.put("member", true);
-            object.put("null_value", JSONObject.NULL);
-            object.put("my_loc_array", array);
-
-
+            // This will be true, since the field "name" is present on our JSON.
+            if (json.has("name")) {
+                name = json.getString("name");
+            } else {
+                name = "John";
+            }
+// This will be false, since our JSON doesn't have the field "surname".
+            if (json.has("surname")) {
+                surname = json.getString("surname");
+            } else {
+                surname = "Doe";
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
 
-        // Calling toString() on the JSONObject returns the JSON in string format.
-        final String json = object.toString();
-        mTextView.setText(json);
-
-
-        // JSONArray nested inside JSONObject
-
-        JSONObject root = null;
-        try {
-            root = new JSONObject(json);
-            JSONArray booksArray = root.getJSONArray("my_loc_array");
-            JSONObject firstBook = booksArray.getJSONObject(0);
-            String title = firstBook.getString("loc1");
-            mParsedTextView.setText(title);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-
-
+        mTextView.setText(name);
     }
 }
